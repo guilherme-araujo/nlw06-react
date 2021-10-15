d48c89cd63ca2b782ed2c604afb294df1b6b8a3c
Guilherme Araújo <guilherme.araujo@imd.ufrn.br>
2021-10-15 20:49:26 -0300

    Athentication ok

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -3,8 +3,11 @@ import logoImg from "../assets/images/logo.svg";
 
 import "../styles/auth.scss";
 import { Button } from "../component/Button";
+import { Link } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
 
 export function NewRoom() {
+  const { user } = useAuth();
   return (
     <div id="page-auth">
       <aside>
@@ -18,6 +21,7 @@ export function NewRoom() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
+          <h1>{user?.name}</h1>
           <h2>Criar uma nova sala</h2>
 
           <form action="">
@@ -25,7 +29,8 @@ export function NewRoom() {
             <Button type="submit">Criar sala</Button>
           </form>
           <p>
-            Quer entrar em uma sala já existente? <a href="#">Clique aqui</a>{" "}
+            Quer entrar em uma sala já existente?{" "}
+            <Link to="/">Clique aqui</Link>{" "}
           </p>
         </div>
       </main>
